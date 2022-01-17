362dafc6acc39f1cb37254c741cee3b43dc2367b
emilio girardi <emilio.g.girardi@gmail.com>
2022-01-17 18:39:14 +0100

    request spec

--- Gemfile
+++ Gemfile
@@ -53,9 +53,6 @@ gem 'solargraph', group: :development
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
-end
-
-group :test do
   gem 'rspec-rails'
 end
 
---
+++ spec/requests/articles_request_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe '/articles', type: :request do
+
+  let(:valid_attributes) do {
+    id: 1,
+    title: 'Hello Rails!',
+    body: 'Im on Rails!',
+  }
+  end
+
+  let(:invalid_attributes) do {
+    id: 'a',
+    title: 'H',
+    body: 'Rails',
+  }
+  end
+
+  describe 'get/index' do
+    it 'renders a successful response' do
+      article = Article.new(valid_attributes)
+      article.save
+      get '/articles'
+      expect(response).to have_http_status(200)
+    end
+  end
+end
