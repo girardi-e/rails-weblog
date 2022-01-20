bdc7d6d428a4a848513cf11e570036c5e1aab931
emilio girardi <emilio.g.girardi@gmail.com>
2022-01-20 15:12:51 +0100

    create Visible concern module

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,15 +1,9 @@
 class Article < ApplicationRecord
+  include Visible
+
   has_many :comments, dependent: :destroy
 
   validates :title, :body, presence: true
   validates :title, length: { in: 2..40 }
   validates :body, length: { in: 10..1000 }
-
-  VALID_STATUSES = [ 'public', 'private', 'archived' ]
-
-  validades :status, inclusion: { in: VALID_STATUSES }
-
-  def archived?
-    status == 'archived'
-  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
+  include Visible
+
   belongs_to :article
 
   validates :commenter, :body, presence: true
@@ -7,12 +9,4 @@ class Comment < ApplicationRecord
     maximum: 255,
     too_long: '%{count} characters is the maximum allowed.'
   }
-
-  VALID_STATUSES = ['public', 'private', 'archived']
-
-  validates :status, inclusion: { in: VALID_STATUSES }
-
-  def archived?
-    status == 'archived'
-  end
 end
