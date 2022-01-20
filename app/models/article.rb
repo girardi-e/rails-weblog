754f06b87d72d3596251ba1a9818448d1ecae21a
emilio girardi <emilio.g.girardi@gmail.com>
2022-01-20 15:41:57 +0100

    new seeds

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,6 @@ class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   validates :title, :body, presence: true
-  validates :title, length: { in: 2..40 }
+  validates :title, length: { in: 2..50 }
   validates :body, length: { in: 10..1000 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database...'
+Article.destroy_all
+
+puts 'Creating Articles ...'
+
+terminal = {
+  title: 'The ultimate terminal',
+  body: 'A step by step guide to setup Hyper with fish and oh-my-posh',
+  status: 'public'
+}
+
+argular = {
+  title: 'Angular Components',
+  body: 'Proven concepts on how to implement and showcase your UI components',
+  status: 'public'
+}
+
+[terminal, angular].each do |attributes|
+  article = Article.create!(attributes)
+  puts "Created #{article.title}"
+end
+
+puts 'Finished!'
